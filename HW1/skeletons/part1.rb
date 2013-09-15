8dc77fb156cad3751c27dc574dd4cd9d101c5dab
pasqualedagostino <pasquale-dagostino@uiowa.edu>
2013-09-14 22:52:48 -0500

    rev 1.3

--- HW1/skeletons/part1.rb
+++ HW1/skeletons/part1.rb
@@ -1,42 +1,23 @@
-#For those just starting out, one suggested way to work on your code
-#is to have a command window open and a text editor with this
-#file loaded.  Make changes to this file, save them, and then
-#run 'ruby part1.rb' at the command line, this will run your program.  Once you're
-#satisfied with your work, save your file
-
-
+# Checks if the string passed to it is a palindrome
+# by taking out everything but the characters of the
+# string to compare it's reverse (case insensitive)
+#
+# @str String contains the palindrome in question
 def palindrome?(str)
-	!!(str.casecmp str.reverse)
+	!!(str.gsub(/[^a-z]/i, "").casecmp(str.gsub(/[^a-z]/i, "").reverse) == 0)
 end
 
+# Converts the string to a string of words, meaning anything
+# that isn't a letter is removed except whitespace. The string
+# is then downcased and split into an array. This array is then
+# hashed and returned in word=>key, count=>value pairings.
+#
+# @str String contains the string to be counted and hashed
+# return Hash holds word=>key, count=>value pairs
 def count_words(str)
-	str.split(' ').length
+	str.gsub!(/[\b[^a-zA-Z ]]/, '')
+	arr = str.downcase.split
+	hash = Hash.new(0)
+	arr.each { |name| hash[name] += 1 }
+	return hash
 end
-
-
-#the code below this line will test your functions. 
-#You should remove everything below this line prior to submitting your file
-
-
-test_str = "there goes the neighborhood"
-
-if palindrome? test_str
-  puts test_str + " is a palindrome!"
-else
-  puts test_str + " is NOT a palindrome!"
-end
-
-
-test_str = "Madam, I'm Adam"
-
-if palindrome? test_str
-  puts test_str + " is a palindrome!"
-else
-  puts test_str + " is NOT a palindrome!"
-end
-
-
-test_str = "The rent is due on the first day of the month unless the first day of the month falls on a Saturday or Sunday"
-
-word_count = count_words test_str
-puts word_count
--- HW1/skeletons/part5.rb
+++ HW1/skeletons/part5.rb
@@ -1,6 +1,17 @@
 class CartesianProduct
   include Enumerable
 
-  # YOUR CODE HERE
+  def initialize (rows, cols)
+  	@rows,@cols = rows,cols
+  end
+
+  def each
+    return to_enum unless block_given?
+    @rows.each do |row| 
+      @cols.each do |col|
+      	yield [row,col]
+      end
+  	end
+  end
 
 end
