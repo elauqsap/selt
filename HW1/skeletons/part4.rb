712617cb8f3f1e3e7ef913b254abe8941d230a2a
pasqualedagostino <pasquale-dagostino@uiowa.edu>
2013-09-16 21:19:35 -0500

    final sub

--- HW1/skeletons/part4.rb
+++ HW1/skeletons/part4.rb
@@ -1,7 +1,7 @@
 # Metaprogramming example to add history to 
 # attr accessor. Stores all the values the
 # accessor has been set to in an array. A
-# reader allows the user to view the history.
+# reader allows the user to view the history
 class Class
 
   def attr_accessor_with_history(attr_name)
@@ -9,13 +9,15 @@ class Class
     attr_reader attr_name        			# create the attribute's getter
     attr_reader attr_name+"_history" 		# create bar_history getter    
     class_eval %Q{
-            def #{attr_name}=(attr_name)
-                if (!defined? @#{attr_name}_history)
-                    @#{attr_name}_history.push(@#{attr_name})
-                end
-                @#{attr_name} = attr_name
-                @#{attr_name}_history.push(attr_name)
-            end
-        }
+        
+        def #{attr_name}=(attr_name)
+            @#{attr_name}_history ||= [nil]
+            @#{attr_name}_history.push(attr_name)
+        end
+
+        def #{attr_name}_history
+            @#{attr_name}_history || [nil]
+        end
+    }
   end
 end
